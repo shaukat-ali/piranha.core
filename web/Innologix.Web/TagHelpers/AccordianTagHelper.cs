cc58b1f2a3659f13d6cf453cc0b52a1b217c3b21
shaukat-ali <shaukat.delhi@gmail.com>
2020-06-08 22:09:06 +0100

    Display templates for new layout blocks

---
+++ web/Innologix.Web/TagHelpers/AccordianTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Piranha.Extend;
+using Innologix.Layouts.Blocks;
+
+namespace Innologix.Web.TagHelpers
+{
+    [HtmlTargetElement("inno-accordian")]
+    public class AccordianTagHelper : TagHelper
+    {
+        public string Id { get; set; }
+        public IList<Block> Items { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            StringBuilder content = new StringBuilder();
+
+            for (var n = 0; n < Items.Count; n++)
+            {
+                var item = Items[n] as BlockGroupItem;
+
+                content.AppendLine($@"
+                    <div>
+                        <a class=""btn btn-link btn-block text-left"" data-toggle=""collapse"" data-target=""#item_{item.Id:N}"" aria-expanded=""{(n == 0 ? "true" : "false")}"">
+                            {item.Title.Value}
+                        </a>
+                        <div id=""item_{item.Id:N}"" class=""collapse {(n == 0 ? "show" : "")}"" aria-labelledby=""headingOne"" data-parent=""#accordian_{Id}"">
+                            {item.Body.Value}
+                        </div>
+                    </div>
+                ");
+            }
+
+            output.TagName = "div";
+            output.Attributes.Add("class", $"block block-group-accordian {(Items.Count > 0 ? "multi" : "")}");
+            output.Attributes.Add("id", $"accordian_{Id}");
+            output.Content.SetHtmlContent(content.ToString());
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
---
+++ web/Innologix.Web/TagHelpers/TabsTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Piranha.Extend;
+using Innologix.Layouts.Blocks;
+
+namespace Innologix.Web.TagHelpers
+{
+    [HtmlTargetElement("inno-tabs")]
+    public class TabsTagHelper : TagHelper
+    {
+        public string Id { get; set; }
+        public IList<Block> Items { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            StringBuilder tagsNav = new StringBuilder();
+            StringBuilder tagsContent = new StringBuilder();
+
+            for (var n = 0; n < Items.Count; n++)
+            {
+                var item = Items[n] as BlockGroupItem;
+                tagsNav.AppendLine($@"
+                    <li class=""nav-item"" role=""presentation"">
+                        <a class=""nav-link {(n == 0 ? "active" : "")}"" data-toggle=""tab"" href=""#tc_{@item.Id:N}"" role=""tab"" aria-selected=""{ (n == 0 ? "true" : "false")}"">{item.Title.Value}</a>
+                    </li>
+                ");
+                tagsContent.AppendLine($@"<div class=""tab-pane fade {(n == 0 ? "show active" : "")}"" id=""tc_{@item.Id:N}"" role=""tabpanel"">{item.Body.Value}</div>");
+            }
+
+            output.TagName = "div";
+            output.Attributes.Add("class", "block block-group-tabs");
+            output.Attributes.Add("id", $"tabs_{Id}");
+            output.Content.SetHtmlContent($@"
+                <ul class=""nav nav-tabs"" role=""tablist"">{ tagsNav }</ul>
+                <div class=""tab-content"">{ tagsContent }</div>
+            ");
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
